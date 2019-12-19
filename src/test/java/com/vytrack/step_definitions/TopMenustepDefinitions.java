6dedf9114308d2dd05d75f9aaee4783f01e43fc6
Harun Erdogan <harunerdogan67@hotmail.com>
2019-12-19 10:30:44 -0600

    New Cucumber Selenium

--- src/test/java/com/vytrack/step_definitions/TopMenustepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/TopMenustepDefinitions.java
@@ -1,7 +1,7 @@
 package com.vytrack.step_definitions;
 
 import com.vytrack.pages.LoginPage;
-import io.cucumber.java.en.Then;
+import io.cucumber.java.en.*;
 
 public class TopMenustepDefinitions {
     LoginPage loginPage = new LoginPage();//created login page object
@@ -9,6 +9,8 @@ public class TopMenustepDefinitions {
 
     @Then("user navigates to {string} then to {string}")
     public void user_navigates_to_then_to(String module, String submodule) {
+        loginPage.navigateTo(module, submodule);
+
 
     }
 
