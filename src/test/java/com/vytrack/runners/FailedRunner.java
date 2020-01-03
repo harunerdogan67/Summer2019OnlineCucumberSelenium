f34856f2b08740f76015a5c5f5971a4779c7aa0d
Harun Erdogan <harunerdogan67@hotmail.com>
2020-01-03 14:24:30 -0600

    New Cucumber Selenium

--- src/test/java/com/vytrack/runners/FailedRunner.java
+++ src/test/java/com/vytrack/runners/FailedRunner.java
@@ -8,10 +8,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "@target/rerun.txt",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
-        plugin = {"html:target/default-cucumber-reports"}
-//                "json:target/cucumber.json"
-//        }
+        plugin = {
+                "html:target/rerun-default-cucumber-reports",
+        }
 )
 public class FailedRunner {
 }
